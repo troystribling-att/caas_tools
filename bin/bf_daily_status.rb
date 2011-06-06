0bfbe9601401530a878dbd3c564e355bd7d92c96
Troy Stribling <troystribling@zenoss4.att>
2011-06-06 17:58:36 -0400

    monitor works. looking into email format.

--- bin/bf_daily_status.rb
+++ bin/bf_daily_status.rb
@@ -3,6 +3,7 @@ this_dir = File.expand_path(File.dirname(__FILE__))
 $:.unshift(this_dir)
 require 'yaml'
 require "#{this_dir}/../lib/cmds"
+require "#{this_dir}/../lib/send_email"
 
 #-------------------------------------------------------------------------------------------------
 CONFIG = File.open(ARGV.first){|yf| YAML::load(yf)}
@@ -19,10 +20,51 @@ agg_response = envs.inject({}) do |agg, env|
                end
 
 #----------------------------------------------------------------------------------------------------
-errors = agg_response.inject({}) do |err, (env, vms)|
-           err[env] = vms.select{|(vm, data)| data[:error]}
-         end
+unreachable_count = 0
+unreachable = agg_response.inject({}) do |unreach, (env, vms)|
+                unreach_vms = vms.select{|vm| vms[vm][:error]}
+                unreachable_count += unreach_vms.size             
+                unreach[env] = unreach_vms               
+                unreach
+              end
+              
+#----------------------------------------------------------------------------------------------------
+reachable_count = 0
+reachable   = agg_response.inject({}) do |reach, (env, vms)|
+                reach_vms = vms.select{|vm| !vms[vm][:error]}
+                reachable_count += reach_vms.size             
+                reach[env] = reach_vms              
+                reach
+              end
          
 #----------------------------------------------------------------------------------------------------
-agg_response.inject("ENVIRONMENT        ERROR MESSAGE           TRANSACTION(MS)         UPTIME") do |s, (env, vms)| 
-end 
+if unreachable_count > 0
+  unreachable_msg = "#{unreachable_count} SERVERS ARE NOT ACCESSIBLE\n" +
+                    "ENVIRONMENT     SERVER                                IP                ERROR\n" +
+                    "--------------------------------------------------------------------------------------------------------------\n"
+  unreachable_msg = unreachable.inject(unreachable_msg) do |msg, (env, vms)| 
+           vms.each do |(vm, data)|
+             msg += sprintf("%-15.15s | %-35.35s | %-15s | %-38.38s\n", env, vm, data[:ip], data[:error_msg])
+           end; msg
+         end 
+else
+  unreachable_msg = "THERE WERE NO ERRORS IN ACCESSING SERVERS\n"
+end         
+
+#----------------------------------------------------------------------------------------------------
+if reachable_count > 0
+  reachable_msg = "#{reachable_count} SERVERS ARE ACCESSIBLE\n" + 
+                  "ENVIRONMENT     SERVER                                IP                TRIES   TRANSACTION(MS)    UPTIME(HRS)\n" +
+                  "--------------------------------------------------------------------------------------------------------------\n"
+  reachable_msg = reachable.inject(reachable_msg) do |msg, (env, vms)| 
+                    vms.each do |(vm, data)|
+                      msg += sprintf("%-15.15s | %-35.35s | %-15s | %-5s | %-16s | %-11s\n", env, vm, data[:ip], data[:tries], data[:elapsed_time_ms], data[:data])
+                    end; msg
+                  end
+else
+  reachable_msg = "NO SERVERS ACCESSIBLE\n"
+end                  
+
+#----------------------------------------------------------------------------------------------------
+msg = "\n" + unreachable_msg + "\n\n\n" + reachable_msg
+send_email(EMAIL_CONFIG['msg']['to'].join(', '), EMAIL_CONFIG['msg']['subject'], msg, EMAIL_CONFIG['server'])
--- lib/send_email.rb
+++ lib/send_email.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'pony' 
 
-def send_email(to,subject, msg, config)
-  Pony.mail(:to => to, :subject => subject, :body => msg, 
-            :via => :smtp, :via_options => {:address => config[:smtp_server], 
-                                            :port => config[:port], 
+def send_email(to, subject, msg, config)
+  Pony.mail(:to => to, :subject => subject, :body => msg, :charset => 'utf-8',
+            :via => :smtp, :via_options => {:address => config["smtp_server"], 
+                                            :port => config["port"], 
                                             :enable_starttls_auto => true, 
-                                            :user_name => config[:user_name], 
-                                            :password => config[:password], 
+                                            :user_name => config["user_name"], 
+                                            :password => config["password"], 
                                             :authentication => :plain, 
                                             :domain => "HELO"})
 end
